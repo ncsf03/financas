c00de5baf81c4f07ba28311a82c1a44710faf136
NIcolas Freitas <143184473+ncsf03@users.noreply.github.com>
2025-05-12 16:02:04 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const mysql = require('mysql2')
+
 const app = express()
 const port = 3000
+
+app.use(bodyParser.urlencoded({ extended: true}))
+app.use(express.static('public'))
+
+const db = mysql.createConnection({
+    host:'localhost',
+    user:'root',    
+    password:'555',    
+    database:'financas'
+})
 
 app.get('/', (req, res) => {
     res.send('Olá Mundo!')
